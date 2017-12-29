44f23c5cd4924e1f4b5f94a4f07095389d5e9a1e
Luis Angel <angeldarkkiller99@live.com>
2017-12-29 12:09:34 -0600

    Estructura basica de modelos y migraciones

---
+++ app/Buyer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+//comprador
+
+//este ya no entiende de Model si no de user, revisar el diagrama de la bd
+
+class Buyer extends User
+{
+    //
+}
---
+++ app/Seller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+//vendedor
+
+//este ya no entiende de Model si no de user, revisar el diagrama de la bd
+
+class Seller extends User
+{
+    //
+}
