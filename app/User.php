62bcfdc9c0c64eb3490f8ffd80a54845861a8682
Luis Angel <angeldarkkiller99@live.com>
2018-01-04 09:40:51 -0600

    Mutadores y accesores para el modelo user

--- app/User.php
+++ app/User.php
@@ -31,6 +31,35 @@ class User extends Authenticatable
         'admin',
     ];
 
+    /*
+     * - Mutadores y accesores en los modeo
+     * - Son metodos que se implementan en los modelos para la modificación de un atributo y  para acceder a dicho valor
+     *
+     * -- Un mutador se utiliza para modificar el valor original de un atributo antes de hacer la insercion en la base de datos
+     *
+     *-- El accesor se utiliza para modificar el valor de un atributo despuesn de haberlo obtenido de la base de datos
+    */
+
+    /*Antes de insertar el nombre requiere que todos los caracteres esten en minuscula excepto el inicial, entonces en este
+      caso vamos a usur un mutador y un accesor*/
+      //mutador
+      public function setNameAttribute($valor){
+        $this->attributes['name'] = strtolower($valor);//para que el nombre siempre se inserte en minuscula
+      }
+
+      //accesor
+      public function getNameAttribute($valor){
+        //con esto estamos retornando el valor del nombre siempre con la primera letra de cada palabra en mayuscula sin la necesidad de modificarlo en la bd
+        return ucwords($valor);
+      }
+
+
+
+      /*Para el correo electronico solo vamos a poner todo en minuscula por lo cual solo usaremos un mutador */
+      public function setEmailAttribute($valor){
+        $this->attributes['email'] = strtolower($valor);//para que el email siempre se inserte en minuscula
+      }
+
     /**
      * The attributes that should be hidden for arrays.
      *
