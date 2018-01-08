58dfb234d16b54b4f76eba9990cba14326c99379
Luis Angel <angeldarkkiller99@live.com>
2018-01-08 10:53:51 -0600

    Implementando las Operaciones para el controlador Category

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -28,7 +28,17 @@ class CategoryController extends ApiController
      */
     public function store(Request $request)
     {
-        //
+      //reglas de validacion
+      $reglas = [
+        'name' => 'required',
+        'description' => 'required',
+      ];
+
+      $this->validate($request, $reglas);
+
+      $category = Category::create($request->all());
+
+      return $this->showOne($category, 201);
     }
 
     /**
@@ -39,7 +49,7 @@ class CategoryController extends ApiController
      */
     public function show(Category $category)
     {
-        //
+        return $this->showOne($category);
     }
 
 
@@ -52,7 +62,23 @@ class CategoryController extends ApiController
      */
     public function update(Request $request, Category $category)
     {
-        //
+        //el metodo fill recibe los valores que vamos actualizar
+        //el metodo insersect recibe unicamente los valores requeridos en este caso es nam e y description
+        $category->fill($request->intersect([
+          'name',
+          'description',
+        ]));
+
+        /*comprobamos si la categoria cambio alguno de sus valores respecto a la instancia original y esto se hace por el
+          metodo isClean() que verifica si la instancia no ah cambiado*/
+          if($category->isClean()){
+            return $this->errorResponse('Debe especificar al menos un valor diferente para actualizar', 422);
+          }
+
+          $category->save();
+
+          return $this->showOne($category);
+
     }
 
     /**
@@ -63,6 +89,8 @@ class CategoryController extends ApiController
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return $this->showOne($category);
     }
 }
