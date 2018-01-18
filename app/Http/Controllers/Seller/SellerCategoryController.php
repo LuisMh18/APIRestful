717da4394f3f5fb3f53a837a88aa860610fd2dc4
Luis Ángel <luis_mh@outlook.es>
2018-01-18 11:53:42 -0600

    obtener la lista de categorias en las que un vendedor a realizado algun tipo de transaccion o venta

--- app/Http/Controllers/Seller/SellerCategoryController.php
+++ app/Http/Controllers/Seller/SellerCategoryController.php
@@ -8,12 +8,12 @@ use App\Http\Controllers\ApiController;
 
 class SellerCategoryController extends ApiController
 {
-    public function __construct()
+  /*  public function __construct()
     {
         parent::__construct();
         $this->middleware('scope:read-general')->only('index');
         $this->middleware('can:view,seller')->only('index');
-    }
+    }*/
     
     /**
      * Display a listing of the resource.
@@ -22,12 +22,13 @@ class SellerCategoryController extends ApiController
      */
     public function index(Seller $seller)
     {
+        //obtener la lista de categorias en las que un vendedor a realizado algun tipo de transaccion o venta
         $categories = $seller->products()
             ->with('categories')
             ->get()
             ->pluck('categories')
             ->collapse()
-            ->unique('id')
+            ->unique('id')//las diferenciamos exclusivamente por el id, es lo mismo a q pongas el unique() sin el id
             ->values();
 
         return $this->showAll($categories);
