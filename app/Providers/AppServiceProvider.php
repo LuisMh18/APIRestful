0eea6b77ae421617cbe4d1dfdd5d4b8415ef6933
Luis Ángel <luis_mh@outlook.es>
2018-01-18 18:48:49 -0600

    Control del estado de un producto dependiendo de su cantidad

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\User;
+use App\Product;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +17,38 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        User::created(function($user) {
+            retry(5, function() use ($user) {
+                Mail::to($user)->send(new UserCreated($user));
+            }, 100);
+        });
+
+        User::updated(function($user) {
+            if ($user->isDirty('email')) {
+                retry(5, function() use ($user) {
+                    Mail::to($user)->send(new UserMailChanged($user));
+                }, 100);
+            }
+        });
+
+
+        /* Control del estado de un producto dependiendo de su cantidad
+          * Lo que queremos hacer es q al momento de que un produto llegue a cantidad de cero su estado cambie 
+          * automaticamente a no disponible porsupuesto si su estado estaba aun en disponible, basicamente esto ocurre cuando un
+          * producto se ah ido comprando sucesivamente por medio de transacciones hasta que su cantidad llega a cero y en ese momento
+          * es cuando queremos que su estado cambie a no disponible, esto lo aremos por eventos del modelo que laravel
+          * automaticamente define y controla por nostros, en este caso aremos uso del evento updated
+         */
+        //decimos que cuando un producto sea actualizado se ejecute lo siguiente
+        //si la cantidad es cero y el producto aun esta disponible debemos cambiarle si estado a no disponible
+        Product::updated(function($product) {
+            if ($product->quantity == 0 && $product->estaDisponible()) {
+                $product->status = Product::PRODUCTO_NO_DISPONIBLE;
+
+                $product->save();
+            }
+        });
     }
 
     /**
