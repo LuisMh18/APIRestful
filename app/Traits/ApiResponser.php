05a113454889288e0c4be4f98f05d6d94c26ae74
Luis Ángel <luis_mh@outlook.es>
2018-01-30 22:21:17 -0600

    Retornando las Nuevas Respuestas Transformadas

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -16,19 +16,42 @@ trait ApiResponser{
   }
 
   //metodo para mostrar una respuesta de elementos
-  public function showAll(Collection $collection, $code = 200){
-      return $this->successResponse(['data' => $collection], $code);
-  }
+  protected function showAll(Collection $collection, $code = 200)
+	{
+    //verificamos si la colección esta vacia
+		if ($collection->isEmpty()) {
+			return $this->successResponse(['data' => $collection], $code);
+		}
+
+		$transformer = $collection->first()->transformer;
+    $collection = $this->transformData($collection, $transformer);
+
+		return $this->successResponse($collection, $code);
+	}
 
   //metodo que mostrara una instancia especifica, por ejemplo cuando tenemos una instancia de un usuario existente
   public function showOne(Model $instance, $code = 200){
-      return $this->successResponse(['data' => $instance], $code);
+    $transformer = $instance->transformer;
+    $instance = $this->transformData($instance, $transformer);
+
+    return $this->successResponse($instance, $code);
   }
 
   
   public function showMessage($message, $code = 200){
     return $this->successResponse(['data' => $message], $code);
-}
+  }
+
+  /* metodo para las transformaciones
+    *recice dos cosas, el primero es la información a transformar y la segunda es la instancia o la clase que se utiizara
+    *para transformar esos datos
+   */
+  protected function transformData($data, $transformer)
+	{
+		$transformation = fractal($data, new $transformer); //construimos las transformación
+
+		return $transformation->toArray();//convertimos la transformación en un array
+	}
 
 }
 
