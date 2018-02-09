6f83a3520ab073fbe8889865f94956ca6adb6b3b
Luis Ángel <luis_mh@outlook.es>
2018-02-09 10:14:49 -0600

    Autenticando Usuarios por Medio de Tokens de la API

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -15,7 +16,7 @@ use App\Transformers\UserTransformer;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, HasApiTokens, SoftDeletes;
 
     const USUARIO_VERIFICADO = '1';
     const USUARIO_NO_VERIFICADO = '0';
