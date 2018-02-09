994b59a2169bc93e063330337d76f6e2180b1fb5
Luis Ángel <luis_mh@outlook.es>
2018-02-09 11:28:30 -0600

    Definiendo el Tiempo de Expiración de los Tokens

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -46,8 +46,16 @@ class AuthServiceProvider extends ServiceProvider
         });
 
         Passport::routes();
-    //    Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
-    //    Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+        /* fecha de expiracion del token, a partir de que se genere el token, 
+        obtenemos la fecha exacta de ese momento por medio de la libreria Carbon y el metodo now, y en este caso le decimos que el token sera
+        valido por 30minutos solamente*/
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(30)); 
+        /*Ahora como el token expira tan pronto es muy prbable que el cliente requiera un refreshToken para generar un nuevo token a partir 
+          de el que ya expiro pero por supuesto no queremos que pueda generar nuevos tokens despues de mucho tiempo asi que también vamos
+          agregarle un tiempo de expiración pero en este caso sera mucho mayor, en este caso sera valido durante 30 días, esto quiere decir entonces que una
+          vez que el token original expira el cliente tiene maximo 30 dias para utilizar un refreshToken y obtener uno nuevo, si no despues de esa fecha tendria
+          que realizar nuevamente el flujo de autorización para obtener un nuevo token por parte del usuario*/
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
     //    Passport::enableImplicitGrant();
 
       /*  Passport::tokensCan([
