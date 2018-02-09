77a8543a6acda181fa884357fcb920a57beaa0dc
Luis Ángel <luis_mh@outlook.es>
2018-02-09 11:49:49 -0600

    Usando client_credentials para Proteger Algunas Rutas

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -12,7 +12,12 @@ class CategoryController extends ApiController
 
   public function __construct()
     {
-       // $this->middleware('client.credentials')->only(['index', 'show']);
+       /*middleware client.credentials para proporcionar acceso a un cliente valido del sistema sin necesidad de que envie o sea autorizado por un usuario, de este
+       modo cualquier cliente registrado en el sistema va poder acceder a las rutas especificadas en el middleware de passport client.credentials, y por supuesto al no tener autorización 
+       o relación con un usuario especifico no podriamos tener un control si por medio de ese token se puede realizar esa acción o no, lo que quiere decir entonces es que
+       vamos a proteger las rutas mas basicas de nuestro sistema, en este caso entonces cualquier usuario, cliente registrado en el sistema deberia poder ver la lista de 
+       productos, la lista de categorias, registrar un nuevo usuario, solicitar reenviar un correo electronico y similar.*/
+       $this->middleware('client.credentials')->only(['index', 'show']);
        // $this->middleware('auth:api')->except(['index', 'show']);
         $this->middleware('transform.input:' . CategoryTransformer::class)->only(['store', 'update']);
     }
--- app/Http/Controllers/Category/CategoryProductController.php
+++ app/Http/Controllers/Category/CategoryProductController.php
@@ -8,6 +8,16 @@ use App\Http\Controllers\ApiController;
 
 class CategoryProductController extends ApiController
 {
+
+    public function __construct()
+    {
+       /*middleware client.credentials para proporcionar acceso a un cliente valido del sistema sin necesidad de que envie o sea autorizado por un usuario, de este
+       modo cualquier cliente registrado en el sistema va poder acceder a las rutas especificadas en el middleware de passport client.credentials, y por supuesto al no tener autorización 
+       o relación con un usuario especifico no podriamos tener un control si por medio de ese token se puede realizar esa acción o no, lo que quiere decir entonces es que
+       vamos a proteger las rutas mas basicas de nuestro sistema, en este caso entonces cualquier usuario, cliente registrado en el sistema deberia poder ver la lista de 
+       productos, la lista de categorias, registrar un nuevo usuario, solicitar reenviar un correo electronico y similar.*/
+       $this->middleware('client.credentials')->only(['index']);
+    }
     /**
      * Display a listing of the resource.
      *
